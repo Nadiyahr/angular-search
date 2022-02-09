0185b12baaf8051135d38ed5313aa67da9f973ad
nadiaH <nadiahrb79@gmailcom>
2022-02-09 13:48:40 +0200

    button groups

--- search-bar/src/app/header/dropdown/dropdown.component.ts
+++ search-bar/src/app/header/dropdown/dropdown.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dropdown',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dropdown.component.scss']
 })
 export class DropdownComponent implements OnInit {
-
+  @Input() title?: string;
   constructor() { }
 
   ngOnInit(): void {
--- search-bar/src/app/header/header.component.ts
+++ search-bar/src/app/header/header.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-  
+  searcBar: boolean = false;
 
   ngOnInit(): void {
   }
